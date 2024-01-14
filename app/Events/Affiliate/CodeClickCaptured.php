53f2d87883e80ba492cf42246cf575837605a69d
Martins Luters <luters.martins@gmail.com>
2024-01-14 16:24:26 +0200

    Add code click captured event
    This will help to process further once affiliate link is captured.
    
    On the event we can add listeners, for example, to create cookie and save event in DB.

---
+++ app/Events/Affiliate/CodeClickCaptured.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Events\Affiliate;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+
+class CodeClickCaptured
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
