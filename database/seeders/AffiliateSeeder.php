d2ae63e782c731a187789b8883e2410d56c010dd
Martins Luters <luters.martins@gmail.com>
2024-01-14 18:45:12 +0200

    Seed: Improve seeder to seed denormalized data
    Will help to speed up manual testing process and showcase the system.

--- database/seeders/AffiliateSeeder.php
+++ database/seeders/AffiliateSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Affiliate\AffiliateCode;
 use App\Models\Affiliate\AffiliateCodeClickEvent;
 use App\Models\Affiliate\AffiliateRegistrationConversion;
+use App\Models\Affiliate\DenormalizedAffiliateCodeClick;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -26,6 +27,43 @@ class AffiliateSeeder extends Seeder
 
         // Some registered regular users via affiliate codes
         AffiliateRegistrationConversion::factory(10)->recycle($affiliate_codes)->create();
+
+        // Insert denormalized data for the 1st affiliate code
+        $click_count = AffiliateCodeClickEvent::where('affiliate_code_id', 1)->count();
+        $conversion_count = AffiliateRegistrationConversion::where('affiliate_code_id', 1)->count();
+        $affiliate_code = AffiliateCode::find(1);
+        (new DenormalizedAffiliateCodeClick([
+            'affiliate_code_id' => 1,
+            'user_id' => 1,
+            'affiliate_code' => $affiliate_code->code,
+            'clicks_count' => $click_count,
+            'conversions_count' => $conversion_count,
+        ]))->save();
+        ;
+
+        // Insert denormalized data for the 2nd affiliate code
+        $click_count = AffiliateCodeClickEvent::where('affiliate_code_id', 2)->count();
+        $conversion_count = AffiliateRegistrationConversion::where('affiliate_code_id', 2)->count();
+        $affiliate_code = AffiliateCode::find(2);
+        (new DenormalizedAffiliateCodeClick([
+            'affiliate_code_id' => 2,
+            'user_id' => 1,
+            'affiliate_code' => $affiliate_code->code,
+            'clicks_count' => $click_count,
+            'conversions_count' => $conversion_count,
+        ]))->save();
+
+        // Insert denormalized data for the 3rd affiliate code
+        $click_count = AffiliateCodeClickEvent::where('affiliate_code_id', 3)->count();
+        $conversion_count = AffiliateRegistrationConversion::where('affiliate_code_id', 3)->count();
+        $affiliate_code = AffiliateCode::find(3);
+        (new DenormalizedAffiliateCodeClick([
+            'affiliate_code_id' => 3,
+            'user_id' => 1,
+            'affiliate_code' => $affiliate_code->code,
+            'clicks_count' => $click_count,
+            'conversions_count' => $conversion_count,
+        ]))->save();
 
         // Some directly registered regular users
         User::factory(10)->create();
