2745a22276547a8344718480e99d9954682a4ec0
Martins Luters <luters.martins@gmail.com>
2024-01-14 16:08:57 +0200

    Add Affiliate service provider

---
+++ app/Providers/Affiliate/AffiliateServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers\Affiliate;
+
+use Illuminate\Support\ServiceProvider;
+
+class AffiliateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
