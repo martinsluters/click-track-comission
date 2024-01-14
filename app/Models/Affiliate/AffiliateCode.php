cd4c37566e34273dc04539351c7ac56661aacafc
Martins Luters <luters.martins@gmail.com>
2024-01-14 11:21:46 +0200

    Model: Add AffiliateCode model
    This will data source for affilite codes.

---
+++ app/Models/Affiliate/AffiliateCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Affiliate;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliateCode extends Model
+{
+    use HasFactory;
+
+    // No timestamps
+    public $timestamps = false;
+
+}
