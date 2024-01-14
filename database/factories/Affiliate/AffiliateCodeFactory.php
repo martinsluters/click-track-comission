6cdc756a814f3503fa262dfb2dbf9decbaf9d22c
Martins Luters <luters.martins@gmail.com>
2024-01-14 11:26:47 +0200

    Factory: Add AffiliateCode factory
    This will be helful for tersting and seeding in future.

---
+++ database/factories/Affiliate/AffiliateCodeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\Affiliate;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Affiliate\AffiliateCode>
+ */
+class AffiliateCodeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->affiliate(),
+            'code' => Str::random(10),
+        ];
+    }
+}
