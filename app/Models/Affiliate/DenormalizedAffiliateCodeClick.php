39799dab2047a192609643142c48226363ef54e0
Martins Luters <luters.martins@gmail.com>
2024-01-14 18:43:59 +0200

    Model: Add DenormalizedAffiliateCodeClick model
    This will data source for analytical data of each affiliate code.

---
+++ app/Models/Affiliate/DenormalizedAffiliateCodeClick.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Affiliate;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DenormalizedAffiliateCodeClick extends Model
+{
+    // No timestamps
+    public $timestamps = false;
+}
