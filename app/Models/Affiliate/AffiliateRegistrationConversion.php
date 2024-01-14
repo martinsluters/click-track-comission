2501670995146e7fa5f3ebfb11a2b09fa83ee2ed
Martins Luters <luters.martins@gmail.com>
2024-01-14 11:23:51 +0200

    Model: Add AffiliateRegistrationConversion model
    This will be the data source for all affiliate related conversions.

---
+++ app/Models/Affiliate/AffiliateRegistrationConversion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Affiliate;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliateRegistrationConversion extends Model
+{
+    use HasFactory;
+
+    // No timestamps
+    public $timestamps = false;
+}
